2b8d3f6b40f43fc76f57b1eb87cb350efbcba0b0
Nursultan11235 <zholnurs@gmail.com>
2020-07-27 17:18:21 +0600

    add controller init

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,14 +2,17 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
 
     ImageView imageViewA, imageViewB, imageViewC, imageViewD;
+    EditText editTextView;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +24,15 @@ public class MainActivity extends AppCompatActivity {
         imageViewB = findViewById(R.id.imageB);
         imageViewC = findViewById(R.id.imageC);
         imageViewD = findViewById(R.id.imageD);
+        editTextView = findViewById(R.id.editTextView);
+        button = findViewById(R.id.button);
 
         assert appbar != null;
         appbar.setTitle("4 фото 1 слово");
+
+        imageViewA.setImageResource(R.drawable.baseball1);
+        imageViewB.setImageResource(R.drawable.baseball1);
+        imageViewC.setImageResource(R.drawable.baseball1);
+        imageViewD.setImageResource(R.drawable.baseball1);
     }
 }
--- app/src/main/java/com/example/myapplication/Question.java
+++ app/src/main/java/com/example/myapplication/Question.java
@@ -2,6 +2,32 @@ package com.example.myapplication;
 
 public class Question {
 
-    String answer;
-    String[] images = new String[4];
+    private String answer;
+    private int[] images;
+
+    Question(int[] images, String answer) {
+        this.answer = answer;
+        this.images = images;
+    }
+
+    static final Question[] QUESTIONS = {
+            new Question(new int[]{R.drawable.baseball1, R.drawable.baseball2, R.drawable.baseball3, R.drawable.baseball4}, "Baseball"),
+            new Question(new int[]{R.drawable.football1, R.drawable.football2, R.drawable.football3, R.drawable.football4}, "Football"),
+            new Question(new int[]{R.drawable.chess1, R.drawable.chess2, R.drawable.chess3, R.drawable.chess4}, "Chess"),
+            new Question(new int[]{R.drawable.bishkek1, R.drawable.bishkek2, R.drawable.bishkek3, R.drawable.bishkek4}, "Bishkek"),
+            new Question(new int[]{R.drawable.france1, R.drawable.france2, R.drawable.france3, R.drawable.france4}, "France"),
+    };
+
+    boolean isRightAnswer(String answer){
+        return this.answer.equals(answer);
+    }
+
+
+    public int[] getImages() {
+        return images;
+    }
+
+    public String getAnswer(){
+        return answer;
+    }
 }
